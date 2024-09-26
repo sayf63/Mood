e2b35d78529bdec3718fdbb7fb8653114481161a
sayf63 <157656643+sayf63@users.noreply.github.com>
2024-09-25 23:29:17 -0500

    Updated the Jump Function
    Allows users to now hold on the space bar to constantly jump where as in before you had to constantly press the space key in order to continuously jump.

--- Movement.gd
+++ Movement.gd
@@ -41,7 +41,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor() :
 		velocity.y = JUMP_VELOCITY
 		
 	#Handle Sprint
